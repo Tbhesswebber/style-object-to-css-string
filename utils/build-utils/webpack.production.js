56556f09619ad5450182a54b4a594445517f3aaa
Tanner B. Hess Webber <tb@hesswebber.com>
2023-11-09 17:18:12 -0500

    chore: removes dead configuration and files

--- utils/build-utils/webpack.production.js
+++
@@ -1,8 +0,0 @@
-// ? creates an environment-oriented webpack config
-module.exports = env => ({
-  output: {
-    filename: 'index.min.js',
-    library: 'styleToCss',
-    libraryTarget: 'umd',
-  },
-});
