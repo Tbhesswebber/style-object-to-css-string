4688c8225149edaf79a901f86d54b304f5dc9a02
Tanner B. Hess Webber <tb@hesswebber.com>
2023-11-09 17:11:19 -0500

    chore: migrates build system to vite (#15)
    * docs: removes unused badges
    
    * chore: removes coverage upload in ci
    
    * chore: migrates build to vite
    
    * chore: migrates node to 18
    
    * chore: updates some deps with npm audit

---
+++ vite.config.mjs
@@ -0,0 +1,24 @@
+import { builtinModules } from "node:module";
+import { resolve } from "node:path";
+
+/** @type {import('vite').UserConfig} */
+export default {
+    build: {
+        lib: {
+            entry: {
+                parsers: resolve(__dirname, "src/parsers.js"),
+                createParser: resolve(__dirname, "src/createParser.js"),
+                main: resolve(__dirname, "src/objToString.js")
+            },
+            name: "styleObjectToCss",
+            fileName: (format, entryAlias) => {
+                const fileName = `index.${format}.js`;
+                return entryAlias === "main" ? fileName : `${entryAlias}/${fileName}`;
+            },
+        },
+        outDir: resolve(__dirname, "dist"),
+        rollupOptions: {
+            external: [...builtinModules, /^node:/],
+        },
+    },
+};
