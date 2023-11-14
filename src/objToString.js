da752e39d139327006837dc68830a162c204ed05
Tanner B. Hess Webber <tb@hesswebber.com>
2023-11-14 12:24:14 -0500

    feat(types): adds basic types via JSDoc comments

--- src/objToString.js
+++ src/objToString.js
@@ -1,5 +1,11 @@
 import { camelToKebab } from './parsers';
 
+/**
+ * Translate a style object into a CSS string
+ * @param {Object} styleObj - The object to run a particular parser against
+ * @param {import('./createParser').Parser} [parser=camelToKebab] - The parser to use for key transformation
+ * @returns {string}
+ */
 function objToString(styleObj, parser = camelToKebab) {
   if (!styleObj || typeof styleObj !== 'object' || Array.isArray(styleObj)) {
     throw new TypeError(`expected an argument of type object, but got ${typeof styleObj}`);
